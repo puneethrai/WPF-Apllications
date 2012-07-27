beb8da8672ec1fea77eee720f3e36fa1940f8281
Puneeth Rai <npuneethrai@yahoo.co.in>
2012-07-28 03:38:13 +0530

    Building it up

--- Games/ChokaBhara-Win8style/ChokaBhara-Win8style/MainWindow.xaml.cs
+++ Games/ChokaBhara-Win8style/ChokaBhara-Win8style/MainWindow.xaml.cs
@@ -22,6 +22,112 @@ namespace ChokaBhara_Win8style
         public MainWindow()
         {
             InitializeComponent();
+            /*
+            StackPanel TitleBarStack = new StackPanel();
+            TitleBarStack.Orientation = Orientation.Horizontal;
+            Image myImage = new Image();
+            BitmapImage myImageSource = new BitmapImage();
+            myImageSource.BeginInit();
+            myImageSource.UriSource = new Uri("autorun.ico");
+            myImageSource.EndInit();
+            myImage.Source = myImageSource;
+
+            TextBlock myTextBlock = new TextBlock();
+            myTextBlock.Text = "This is my image";
+
+            TitleBarStack.Children.Add(myImage);
+            TitleBarStack.Children.Add(myTextBlock);
+
+            TitleGrid.Children.Add(TitleBarStack);*/
+            BitmapImage logo = new BitmapImage();
+            logo.BeginInit();
+            logo.UriSource = new Uri(@"C:\Users\gangathara rai\Documents\GitHub\WPF-Apllications\Games\ChokaBhara-Win8style\ChokaBhara-Win8style\autorun.ico");
+            
+            logo.EndInit();
+            AppIcon.Source = logo;
         }
+
+        #region WindowControRegion
+
+        private void WinControl_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (sender.Equals(MinimizeLine))
+            {
+                Console.WriteLine("Minimize");
+                MinimizeLine.Stroke = Brushes.Blue;
+            }
+            else
+            {
+                Console.WriteLine("Close");
+                CloseLine1.Stroke = Brushes.Blue;
+                CloseLine2.Stroke = Brushes.Blue;
+            }
+        }
+
+        private void WinControl_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (sender.Equals(MinimizeLine))
+            {
+                Console.WriteLine("Minimize");
+                MinimizeLine.Stroke = Brushes.Black;
+                MinimizeRect.Fill = Brushes.White;
+            }
+            else
+            {
+                Console.WriteLine("Close");
+                CloseLine1.Stroke = Brushes.Black;
+                CloseLine2.Stroke = Brushes.Black;
+                CloseRect.Fill = Brushes.White;
+             
+            }
+        }
+
+        private void WinControl_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (sender.Equals(MinimizeLine))
+            {
+                Console.WriteLine("Minimize");
+                MinimizeRect.Fill = Brushes.White;
+                MinimizeLine.Stroke = Brushes.Blue;
+            }
+            else
+            {
+                Console.WriteLine("Close");
+                CloseRect.Fill = Brushes.White;
+                CloseLine1.Stroke = Brushes.Red;
+                CloseLine2.Stroke = Brushes.Red;
+                
+                App.Current.Shutdown();
+            } 
+        }
+
+        private void WinControl_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender.Equals(MinimizeLine))
+            {
+                Console.WriteLine("Minimize");
+                MinimizeLine.Stroke = Brushes.White;
+                MinimizeRect.Fill = Brushes.Blue;
+            }
+            else
+            {
+                Console.WriteLine("Close");
+                CloseRect.Fill = Brushes.Red;
+                CloseLine1.Stroke = Brushes.White;
+                CloseLine2.Stroke = Brushes.White;
+            }
+        }
+        #endregion
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TitleGrid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
     }
 }
