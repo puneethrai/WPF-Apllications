079b9c2ecadf4d8813498d382457c9867160a37d
Puneeth Rai <npuneethrai@yahoo.co.in>
2012-07-28 15:21:49 +0530

    Just see how the end product will be

---
+++ Games/ChokaBhara-Win8style/ChokaBhara-Win8style/GifAnimation.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+using System.Windows.Threading;
+using System.Windows.Interop;
+
+namespace ChokaBhara_Win8style
+{
+    public class AnimatedImageControl : UserControl
+    {
+        delegate void OnFrameChangedDelegate();
+        System.Drawing.Image m_animatedImage;
+        HwndSource m_hwnd;
+        System.Drawing.Rectangle m_rectangle;
+        System.Windows.Point m_point;
+        System.Drawing.Brush m_brush;
+
+        public AnimatedImageControl(Window p_window, Bitmap p_bitmap, System.Drawing.Brush p_brush)
+        {
+            m_animatedImage = p_bitmap;
+            m_hwnd = PresentationSource.FromVisual((Visual)p_window) as HwndSource;
+            m_brush = p_brush;
+            Width = p_bitmap.Width;
+            Height = p_bitmap.Height;
+            m_point = p_window.PointToScreen(new System.Windows.Point(0, 0));
+
+            ImageAnimator.Animate(m_animatedImage, new EventHandler(OnFrameChanged));
+
+            Loaded += new RoutedEventHandler(AnimatedImageControl_Loaded);
+        }
+
+        void AnimatedImageControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Point point = this.PointToScreen(new System.Windows.Point(0, 0));
+            m_rectangle = new System.Drawing.Rectangle((int)(point.X - m_point.X), (int)(point.Y - m_point.Y), (int)Width, (int)Height);
+        }
+
+        void OnFrameChangedTS()
+        {
+            InvalidateVisual();
+        }
+
+        void OnFrameChanged(object o, EventArgs e)
+        {
+            Dispatcher.BeginInvoke(DispatcherPriority.Normal, new OnFrameChangedDelegate(OnFrameChangedTS));
+        }
+
+        protected override void OnRender(DrawingContext p_drawingContext)
+        {
+            //Get the next frame ready for rendering.
+            ImageAnimator.UpdateFrames(m_animatedImage);
+
+            //Draw the next frame in the animation.
+            Graphics gr = Graphics.FromHwnd(m_hwnd.Handle);
+            gr.FillRectangle(m_brush, m_rectangle);
+            gr.DrawImage(m_animatedImage, m_rectangle);
+        }
+    }
+}
--- Games/ChokaBhara-Win8style/ChokaBhara-Win8style/MainWindow.xaml.cs
+++ Games/ChokaBhara-Win8style/ChokaBhara-Win8style/MainWindow.xaml.cs
@@ -38,20 +38,25 @@ namespace ChokaBhara_Win8style
             TitleBarStack.Children.Add(myImage);
             TitleBarStack.Children.Add(myTextBlock);
 
-            TitleGrid.Children.Add(TitleBarStack);*/
+            TitleGrid.Children.Add(TitleBarStack);
             BitmapImage logo = new BitmapImage();
             logo.BeginInit();
-            logo.UriSource = new Uri(@"C:\Users\gangathara rai\Documents\GitHub\WPF-Apllications\Games\ChokaBhara-Win8style\ChokaBhara-Win8style\autorun.ico");
-            
+            logo.UriSource = new Uri(@"..\..\autorun.ico",UriKind.Relative);
+            logo.DecodePixelHeight = 19;
+            logo.DecodePixelWidth = 19;
             logo.EndInit();
-            AppIcon.Source = logo;
+            AppIcon.Source = logo;*/
         }
 
         #region WindowControRegion
 
+        private void TitleGrid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
         private void WinControl_MouseEnter(object sender, MouseEventArgs e)
         {
-            if (sender.Equals(MinimizeLine))
+            if (sender.Equals(MinimizeLine)||sender.Equals(MinimizeRect))
             {
                 Console.WriteLine("Minimize");
                 MinimizeLine.Stroke = Brushes.Blue;
@@ -59,14 +64,14 @@ namespace ChokaBhara_Win8style
             else
             {
                 Console.WriteLine("Close");
-                CloseLine1.Stroke = Brushes.Blue;
-                CloseLine2.Stroke = Brushes.Blue;
+                CloseLine1.Stroke = Brushes.Red;
+                CloseLine2.Stroke = Brushes.Red;
             }
         }
 
         private void WinControl_MouseLeave(object sender, MouseEventArgs e)
         {
-            if (sender.Equals(MinimizeLine))
+            if (sender.Equals(MinimizeLine) || sender.Equals(MinimizeRect))
             {
                 Console.WriteLine("Minimize");
                 MinimizeLine.Stroke = Brushes.Black;
@@ -84,7 +89,7 @@ namespace ChokaBhara_Win8style
 
         private void WinControl_MouseUp(object sender, MouseButtonEventArgs e)
         {
-            if (sender.Equals(MinimizeLine))
+            if (sender.Equals(MinimizeLine) || sender.Equals(MinimizeRect))
             {
                 Console.WriteLine("Minimize");
                 MinimizeRect.Fill = Brushes.White;
@@ -103,7 +108,7 @@ namespace ChokaBhara_Win8style
 
         private void WinControl_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            if (sender.Equals(MinimizeLine))
+            if (sender.Equals(MinimizeLine) || sender.Equals(MinimizeRect))
             {
                 Console.WriteLine("Minimize");
                 MinimizeLine.Stroke = Brushes.White;
@@ -121,13 +126,75 @@ namespace ChokaBhara_Win8style
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            Console.WriteLine("Loaded");
+            //this.pictureBoxLoading.Image = System.Drawing.Image.FromFile(@"C:\Users\gangathara rai\Documents\GitHub\WPF-Apllications\Games\ChokaBhara-Win8style\ChokaBhara-Win8style\animated_loader.gif");
+            // Create a collection of points for a polygon
+             System.Windows.Point Point1 = new System.Windows.Point(60,40);
+             System.Windows.Point Point2 = new System.Windows.Point(70,50);
+             System.Windows.Point Point3 = new System.Windows.Point(80,40);
+             System.Windows.Point Point4 = new System.Windows.Point(60,40);
+             PointCollection polygonPoints = new PointCollection();
+             polygonPoints.Add(Point1);
+             polygonPoints.Add(Point2);
+             polygonPoints.Add(Point3);
+             polygonPoints.Add(Point4);
 
+             TurnDisplayTri.Points = polygonPoints;
+            
+             for (long i = 0; i < 999999999; i++) ;
+             Point1 = new System.Windows.Point(120, 40);
+             Point2 = new System.Windows.Point(130, 50);
+             Point3 = new System.Windows.Point(140, 40);
+             Point4 = new System.Windows.Point(120, 40);
+             polygonPoints = new PointCollection();
+             polygonPoints.Add(Point1);
+             polygonPoints.Add(Point2);
+             polygonPoints.Add(Point3);
+             polygonPoints.Add(Point4);
+             TurnDisplayTri.Points = polygonPoints;
+             for (long i = 0; i < 999999999; i++) ;
+             Point1 = new System.Windows.Point(180, 40);
+             Point2 = new System.Windows.Point(190, 50);
+             Point3 = new System.Windows.Point(200, 40);
+             Point4 = new System.Windows.Point(180, 40);
+             polygonPoints = new PointCollection();
+             polygonPoints.Add(Point1);
+             polygonPoints.Add(Point2);
+             polygonPoints.Add(Point3);
+             polygonPoints.Add(Point4);
+             TurnDisplayTri.Points = polygonPoints;
+             for (long i = 0; i < 999999999; i++) ;
+             Point1 = new System.Windows.Point(0, 40);
+             Point2 = new System.Windows.Point(10, 50);
+             Point3 = new System.Windows.Point(20, 40);
+             Point4 = new System.Windows.Point(0, 40);
+             polygonPoints = new PointCollection();
+             polygonPoints.Add(Point1);
+             polygonPoints.Add(Point2);
+             polygonPoints.Add(Point3);
+             polygonPoints.Add(Point4);
+             TurnDisplayTri.Points = polygonPoints;
+             Console.WriteLine("DOne");
+             System.Windows.Media.Brush brush = Background;
+             if (m_grid.Background != System.Windows.Media.Brushes.Transparent && m_grid.Background != null)
+                 brush = m_grid.Background;
+
+             System.Windows.Media.SolidColorBrush scb = (System.Windows.Media.SolidColorBrush)brush;
+             System.Drawing.Color color = System.Drawing.Color.White;
+             if (scb != null)
+                 color = System.Drawing.Color.FromArgb(scb.Color.A, scb.Color.R, scb.Color.G, scb.Color.B);
+
+             System.Drawing.Brush solidBrush = new System.Drawing.SolidBrush(color);
+
+             //
+             AnimatedImageControl animatedImageControl =
+                 new AnimatedImageControl(this, ChokaBhara_Win8style.Properties.Resources.animated_loader, solidBrush);
+             m_grid.Children.Add(animatedImageControl);
+             m_grid.HorizontalAlignment = HorizontalAlignment.Left;
+             m_grid.VerticalAlignment = VerticalAlignment.Top;
+             animatedImageControl.Margin = new Thickness(30, 10, 30, 10);
         }
 
-        private void TitleGrid_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            this.DragMove();
-        }
 
     }
 }
