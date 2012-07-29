f30bde40ff5a50330130c8dbb262792f43da7e36
Puneeth Rai <npuneethrai@yahoo.co.in>
2012-07-29 21:12:54 +0530

    Added the grid & movement logic

--- Games/ChokaBhara-Win8style/ChokaBhara-Win8style/MainWindow.xaml.cs
+++ Games/ChokaBhara-Win8style/ChokaBhara-Win8style/MainWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Threading;
 
 namespace ChokaBhara_Win8style
 {
@@ -19,9 +21,13 @@ namespace ChokaBhara_Win8style
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int TurnState;
+        private int DiceNo;
+        private Random DiceRand;
         public MainWindow()
         {
             InitializeComponent();
+            DiceRand = new Random();
             /*
             StackPanel TitleBarStack = new StackPanel();
             TitleBarStack.Orientation = Orientation.Horizontal;
@@ -127,32 +133,19 @@ namespace ChokaBhara_Win8style
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             Console.WriteLine("Loaded");
-            //this.pictureBoxLoading.Image = System.Drawing.Image.FromFile(@"C:\Users\gangathara rai\Documents\GitHub\WPF-Apllications\Games\ChokaBhara-Win8style\ChokaBhara-Win8style\animated_loader.gif");
+            //this.pictureBoxLoading.Image = System.Drawing.Image.FromFile(@"C:\Users\gangathara rai\Documents\GitHub\WPF-Apllications\Games\ChokaBhara-Win8style\ChokaBhara-Win8style\bin\Debug\animated_loader.gif");
+            System.Drawing.Bitmap animatedImage = new System.Drawing.Bitmap("SampleAnimation.gif");
+            System.Drawing.ImageAnimator.Animate(animatedImage, new EventHandler(this.OnFrameChanged))
             
-             Console.WriteLine("DOne");
-             System.Windows.Media.Brush brush = Background;
-             if (m_grid.Background != System.Windows.Media.Brushes.Transparent && m_grid.Background != null)
-                 brush = m_grid.Background;
-
-             System.Windows.Media.SolidColorBrush scb = (System.Windows.Media.SolidColorBrush)brush;
-             System.Drawing.Color color = System.Drawing.Color.White;
-             if (scb != null)
-                 color = System.Drawing.Color.FromArgb(scb.Color.A, scb.Color.R, scb.Color.G, scb.Color.B);
-
-             System.Drawing.Brush solidBrush = new System.Drawing.SolidBrush(color);
-
-             //
-             AnimatedImageControl animatedImageControl =
-                 new AnimatedImageControl(this, ChokaBhara_Win8style.Properties.Resources.animated_loader, solidBrush);
-             m_grid.Children.Add(animatedImageControl);
-             m_grid.HorizontalAlignment = HorizontalAlignment.Left;
-             m_grid.VerticalAlignment = VerticalAlignment.Top;
-             animatedImageControl.Margin = new Thickness(30, 10, 30, 10);
         }
-        private int TurnState;
+        
         private void button1_Click(object sender, RoutedEventArgs e)
         {
             // Create a collection of points for a polygon
+            Turn();
+        }
+        void Turn()
+        {
             switch (TurnState)
             {
                 case 0:
@@ -218,7 +211,155 @@ namespace ChokaBhara_Win8style
                     break;
             }
         }
+        RadioButton TempRadio1;
+        RadioButton TempRadio2;
+        RadioButton TempRadio3;
+        RadioButton TempRadio4;
+        StackPanel TempStackpanel;
+        private void DiceBtn_Click(object sender, RoutedEventArgs e)
+        {
+            DiceNo = DiceRand.Next(1, 8);
+            DiceNoLabel.Content = "" + DiceNo;
+            KayiGrid.Background = Brushes.Magenta;
+            TempStackpanel = new StackPanel();
+            KayiGrid.Children.Add(TempStackpanel);
+            TempRadio1 = new RadioButton();
+            TempRadio1.Name = "TempRadio1";
+            TempRadio1.Content = "Kayi 1";
+            TempRadio2 = new RadioButton();
+            TempRadio2.Name = "TempRadio2";
+            TempRadio2.Content = "Kayi 2";
+            TempRadio3 = new RadioButton();
+            TempRadio3.Name = "TempRadio3";
+            TempRadio3.Content = "Kayi 3";
+            TempRadio4 = new RadioButton();
+            TempRadio4.Name = "TempRadio4";
+            TempRadio4.Content = "Kayi 4";
+            TempRadio1.Checked += new RoutedEventHandler(TempRadio_Checked);
+            TempRadio2.Checked += new RoutedEventHandler(TempRadio_Checked);
+            TempRadio3.Checked += new RoutedEventHandler(TempRadio_Checked);
+            TempRadio4.Checked += new RoutedEventHandler(TempRadio_Checked);
+            TempStackpanel.Children.Add(TempRadio1);
+            TempStackpanel.Children.Add(TempRadio2);
+            TempStackpanel.Children.Add(TempRadio3);
+            TempStackpanel.Children.Add(TempRadio4);
+            
+        }
+        Rectangle[,] MoveRect;
+        void TempRadio_Checked(object sender, RoutedEventArgs e)
+        {
+            if (sender.Equals(TempRadio1))
+            {
+                if((bool)TempRadio1.IsChecked)
+                MessageBox.Show("Checked 1");
+            }
+            else if (sender.Equals(TempRadio2))
+            {
+                if ((bool)TempRadio2.IsChecked)
+                    MessageBox.Show("Checked 2");
+            }
+            else if (sender.Equals(TempRadio3))
+            {
+                if ((bool)TempRadio3.IsChecked)
+                    MessageBox.Show("Checked 3");
+            }
+            else if (sender.Equals(TempRadio4))
+            {
+                if ((bool)TempRadio4.IsChecked)
+                    MessageBox.Show("Checked 4");
+            }
+            
+            KayiGrid.Children.Remove(TempStackpanel);
+            KayiGrid.Background = Brushes.White;
+           
+            
+            MoveRect = new Rectangle[,]
+            {
+                {R13,R12,R11,R21,R31,R41,R51,R52,R53,R54,R55,R45,R35,R25,R15,R14,R24,R34,R44,R43,R42,R32,R22,R23,R33},
+                {R35,R25,R15,R14,R13,R12,R11,R21,R31,R41,R51,R52,R53,R54,R55,R45,R44,R43,R42,R32,R22,R23,R24,R34,R33},
+                {R53,R54,R55,R45,R35,R25,R15,R14,R13,R12,R11,R21,R31,R41,R51,R52,R42,R32,R22,R23,R24,R34,R44,R43,R33},
+                {R31,R41,R51,R52,R53,R54,R55,R45,R35,R25,R15,R14,R13,R12,R11,R21,R22,R23,R24,R34,R44,R43,R42,R32,R33}
+            };
 
+            // Create an Ellipse
+            Ellipse blueRectangle = new Ellipse();
+            blueRectangle.Height = 100;
+            blueRectangle.Width = 200;
+
+            // Create a blue and a black Brush
+            SolidColorBrush blueBrush = new SolidColorBrush();
+            blueBrush.Color = Colors.Blue;
+            SolidColorBrush blackBrush = new SolidColorBrush();
+            blackBrush.Color = Colors.Black;
+
+            // Set Ellipse's width and color
+            blueRectangle.StrokeThickness = 4;
+            blueRectangle.Stroke = blackBrush;
+            // Fill rectangle with blue color
+            blueRectangle.Fill = blueBrush;
+            blueRectangle.HorizontalAlignment = MoveRect[0, 0].HorizontalAlignment;
+            blueRectangle.VerticalAlignment = MoveRect[0, 0].VerticalAlignment;
+             blueRectangle.Margin = MoveRect[0, 0].Margin;
+            // Add Ellipse to the Grid.
+            RectCanvas.Children.Add(blueRectangle);
+            ThreadStart start = new ThreadStart(Start);
+            Thread one = new Thread(start);
+            one.Start();
+            if (first)
+            {
+
+                first = false;
+                
+                temp1 = MoveRect[0, 0].Fill;
+                temp2 = MoveRect[1, 0].Fill;
+                temp3 = MoveRect[2, 0].Fill;
+                temp4 = MoveRect[3, 0].Fill;
+            }
+        }
+        bool first = true;
+        Brush temp1;
+        Brush temp2;
+        Brush temp3;
+        Brush temp4;
+        Ellipse blueRectangle;
+        void Start()
+        {
+
+            
+            
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 25; j++)
+                {
+
+                    MoveRect[i, j].Dispatcher.BeginInvoke((ThreadStart)delegate()
+                    {
+                        //blueRectangle.HorizontalAlignment = MoveRect[i, j].HorizontalAlignment;
+                        //blueRectangle.VerticalAlignment = MoveRect[i, j].VerticalAlignment;
+                       // blueRectangle.Margin = MoveRect[i, j].Margin;
+                        // Add Ellipse to the Grid.
+                        //RectCanvas.Children.Add(blueRectangle);
+                            switch (i)
+                            {
+                                case 0: MoveRect[i, DiceNo].Fill = temp1;
+                                    break;
+                                case 1: MoveRect[i, DiceNo].Fill = temp2;
+                                    break;
+                                case 2: MoveRect[i, DiceNo].Fill = temp3;
+                                    break;
+                                case 3: MoveRect[i, DiceNo].Fill = temp4;
+                                    break;
+                            }
+                            
+                           // RectCanvas.Children.Remove(blueRectangle);
+                        });
+                    for (long k = 0; k < 99999999; k++) ;
+                }
+                Dispatcher.BeginInvoke((ThreadStart)delegate()
+                { Turn(); });
+            }
+            
+        }
 
 
     }
---
+++ Games/TestGifanimae/TestGifanimae/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TestGifanimae
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Bitmap animatedImage = new Bitmap(@"C:\Users\gangathara rai\Documents\GitHub\WPF-Apllications\Games\ChokaBhara-Win8style\ChokaBhara-Win8style\bin\Debug\animated_loader.gif");
+        bool currentlyAnimating = false;
+        private void pictureBox1_Click(object sender, System.EventArgs e)
+        {
+            this.pictureBox1.Image = System.Drawing.Image.FromFile(@"C:\Users\gangathara rai\Documents\GitHub\WPF-Apllications\Games\ChokaBhara-Win8style\ChokaBhara-Win8style\bin\Debug\animated_loader.gif");
+        }
+        //This method begins the animation.
+        public void AnimateImage()
+        {
+            if (!currentlyAnimating)
+            {
+
+                //Begin the animation only once.
+               // ImageAnimator.Animate(animatedImage, new EventHandler(this.OnFrameChanged));
+                currentlyAnimating = true;
+            }
+        }
+        private void OnFrameChanged(object o, EventArgs e)
+        {
+
+            //Force a call to the Paint event handler.
+
+            this.pictureBox1.Image = animatedImage; 
+            AnimateImage();
+
+            //Get the next frame ready for rendering.
+            ImageAnimator.UpdateFrames();
+        }
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            AnimateImage();
+        }
+    }
+
+
+public class animateImage : Form 
+{
+
+    //Create a Bitmpap Object.
+    Bitmap animatedImage = new Bitmap(@"C:\Users\gangathara rai\Documents\GitHub\WPF-Apllications\Games\ChokaBhara-Win8style\ChokaBhara-Win8style\bin\Debug\animated_loader.gif");
+    bool currentlyAnimating = false;
+
+    //This method begins the animation.
+    public void AnimateImage() 
+    {
+        if (!currentlyAnimating) 
+        {
+
+            //Begin the animation only once.
+            ImageAnimator.Animate(animatedImage, new EventHandler(this.OnFrameChanged));
+            currentlyAnimating = true;
+        }
+    }
+
+    private void OnFrameChanged(object o, EventArgs e) 
+    {
+
+        //Force a call to the Paint event handler.
+
+        AnimateImage();
+
+        //Get the next frame ready for rendering.
+        ImageAnimator.UpdateFrames();
+    }
+
+    protected override void OnPaint(PaintEventArgs e) 
+    {
+
+        //Begin the animation.
+        AnimateImage();
+
+        //Get the next frame ready for rendering.
+        ImageAnimator.UpdateFrames();
+
+        //Draw the next frame in the animation.
+        e.Graphics.DrawImage(this.animatedImage, new Point(0, 0));
+    }
+
+    public static void Main() 
+    {
+        Application.Run(new animateImage());
+        
+    }
+}
+}
