767e6221aaf69a46ef696d55937924dfb367eb81
Puneeth Rai <npuneethrai@yahoo.co.in>
2012-09-03 00:50:11 +0530

    Server Client in progress

--- Games/ChokaBhara-Win8style/ChokaBhara-Win8style/GifAnimation.cs
+++ Games/ChokaBhara-Win8style/ChokaBhara-Win8style/GifAnimation.cs
@@ -17,54 +17,52 @@ using System.Windows.Interop;
 
 namespace ChokaBhara_Win8style
 {
-    public class AnimatedImageControl : UserControl
+    public partial class MainWindow 
     {
-        delegate void OnFrameChangedDelegate();
-        System.Drawing.Image m_animatedImage;
-        HwndSource m_hwnd;
-        System.Drawing.Rectangle m_rectangle;
-        System.Windows.Point m_point;
-        System.Drawing.Brush m_brush;
-
-        public AnimatedImageControl(Window p_window, Bitmap p_bitmap, System.Drawing.Brush p_brush)
+        private void GifActions()
         {
-            m_animatedImage = p_bitmap;
-            m_hwnd = PresentationSource.FromVisual((Visual)p_window) as HwndSource;
-            m_brush = p_brush;
-            Width = p_bitmap.Width;
-            Height = p_bitmap.Height;
-            m_point = p_window.PointToScreen(new System.Windows.Point(0, 0));
-
-            ImageAnimator.Animate(m_animatedImage, new EventHandler(OnFrameChanged));
-
-            Loaded += new RoutedEventHandler(AnimatedImageControl_Loaded);
+            ServerConnect.LoadedBehavior = MediaState.Manual;
+            ServerConnect.Play();
+            ServerConnect.MediaEnded += new RoutedEventHandler(GIF_MediaEnded);
+            WaitingGIF.LoadedBehavior = MediaState.Manual;
+            WaitingGIF.Play();
+            WaitingGIF.MediaEnded += new RoutedEventHandler(GIF_MediaEnded);
         }
-
-        void AnimatedImageControl_Loaded(object sender, RoutedEventArgs e)
+        private void GIF_MediaEnded(object sender, RoutedEventArgs REA)
         {
-            System.Windows.Point point = this.PointToScreen(new System.Windows.Point(0, 0));
-            m_rectangle = new System.Drawing.Rectangle((int)(point.X - m_point.X), (int)(point.Y - m_point.Y), (int)Width, (int)Height);
-        }
+            if (sender.Equals(ServerConnect))
+            {
+                if (ServerConnectionStatus != (UInt16)eServerConnectionStatus.CONNECTED)
+                {
+                    ServerConnect.Visibility = Visibility.Visible;
+                    ServerConnect.LoadedBehavior = MediaState.Manual;
+                    ServerConnect.Position = TimeSpan.FromSeconds(5);
+                    ServerConnect.Play();
 
-        void OnFrameChangedTS()
-        {
-            InvalidateVisual();
-        }
+                }
+                else
+                {
+                    ServerConnect.Stop();
+                    ServerConnect.Visibility = Visibility.Hidden;
+                }
+            }
+            else
+            {
+                if (PlayerStatus == (UInt16)ePlayerStatus.OthersTurn)
+                {
+                    WaitingGIF.Visibility = Visibility.Visible;
+                    WaitingGIF.LoadedBehavior = MediaState.Manual;
+                    WaitingGIF.Position = TimeSpan.FromSeconds(5);
+                    WaitingGIF.Play();
 
-        void OnFrameChanged(object o, EventArgs e)
-        {
-            Dispatcher.BeginInvoke(DispatcherPriority.Normal, new OnFrameChangedDelegate(OnFrameChangedTS));
-        }
+                }
+                else
+                {
+                    WaitingGIF.Stop();
+                    WaitingGIF.Visibility = Visibility.Hidden;
 
-        protected override void OnRender(DrawingContext p_drawingContext)
-        {
-            //Get the next frame ready for rendering.
-            ImageAnimator.UpdateFrames(m_animatedImage);
-
-            //Draw the next frame in the animation.
-            Graphics gr = Graphics.FromHwnd(m_hwnd.Handle);
-            gr.FillRectangle(m_brush, m_rectangle);
-            gr.DrawImage(m_animatedImage, m_rectangle);
+                }
+            }
         }
     }
 }
