4ee2aa60e61280e8fb5d00d48057d6d7957d9e33
Puneeth Rai <npuneethrai@yahoo.co.in>
2012-09-03 00:50:28 +0530

    Server Client in progress

---
+++ Games/ChokaBhara-Win8style/ChokaBhara-Win8style/ClientSocket.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Configuration;
+namespace ChokaBhara_Win8style
+{
+    public partial class MainWindow
+    {
+        Socket ClientSocket = null;
+        byte[] RecvBuffer = null;
+        private bool ConnectToServer()
+        {
+            bool Connected = false;
+            
+            ServerConnectionStatus = (UInt16)eServerConnectionStatus.STARTING;
+            if (RecvBuffer == null)
+            {
+                RecvBuffer = new byte[1024];
+            }
+            if (ClientSocket == null) 
+            {
+                
+                ClientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream,ProtocolType.Tcp);
+                ClientSocket.BeginConnect(ServerAddress, ServerPort, (IAsyncResult ar) =>
+                    {
+                        
+                        if (ClientSocket.Connected)
+                        {
+                            ServerConnectionStatus = (UInt16)eServerConnectionStatus.CONNECTED;
+                            ClientSocket.BeginReceive(RecvBuffer, 0, RecvBuffer.Length, 0, SocketReceive, null);
+                            Connected = true;
+                            Console.WriteLine("Conected to server");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Failed to Conect to server" + ServerAddress);
+                            ServerConnectionStatus = (UInt16)eServerConnectionStatus.ERROR;
+                            ClientSocket = null;
+                            Connected = false;
+                        }
+                    }, null);
+                
+                
+            }
+            return Connected;
+        }
+        private void SocketReceive(IAsyncResult ar)
+        {
+            string sRecvBuffer = Encoding.ASCII.GetString(RecvBuffer);
+        }
+    }
+}
---
+++ Games/ChokaBhara-Win8style/ChokaBhara-Win8style/ReadConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+namespace ChokaBhara_Win8style
+{
+    
+    public partial class MainWindow
+    {
+        public string ServerAddress = null;
+        public int ServerPort = 0;
+        public void ReadConfig()
+        {
+            ServerAddress = ConfigurationManager.AppSettings["ServerAddress"];
+            ServerPort = Convert.ToInt32(ConfigurationManager.AppSettings["ServerPort"]);
+        }
+
+    }
+}
